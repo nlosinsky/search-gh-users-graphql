025a37808e9be187e8c911ffe01b541522a8f741
Mykola Losinskyi <nick.losinskii@gmail.com>
2025-06-08 18:48:19 +0300

    defined get user query

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,8 +1,36 @@
+import { GET_USER } from '@/queries';
+import { UserData } from '@/types';
+import { useQuery } from '@apollo/client';
+
 type UserProfileProps = {
   userName: string;
 };
 
-const UserProfile = ({ userName }: UserProfileProps) => {
-  return <h1 className='text-2xl font-bold'>{userName}</h1>;
+const UserProfile = ({userName}: UserProfileProps) => {
+  const {loading, error, data} = useQuery<UserData>(GET_USER, {
+    variables: {login: userName},
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <h2 className='text-xl'>{error.message}</h2>;
+  if (!data) return <h2 className='text-xl'>User Not Found.</h2>;
+
+  const {
+    avatarUrl,
+    name,
+    bio,
+    url,
+    repositories,
+    followers,
+    following,
+    gists,
+  } = data.user;
+  console.log(data);
+
+  return (
+    <div>
+      <h1>{bio}</h1>
+    </div>
+  );
 };
 export default UserProfile;
